f4825f15286aa2fc5cba1d14f6c2811d8f08f97e
loewentj <38169321+loewentj@users.noreply.github.com>
2020-01-02 13:01:14 +1100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const clock = document.querySelector('main .jumbotron .clock');
+const todaysDate = document.querySelector('main .jumbotron .date');
+const getDate = new Date();
+
+
+
+const tick = () => {
+
+    const now = new Date();
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const minutes = m > 9 ? m : '0' + m;
+    const s = now.getSeconds();
+    const seconds = s > 9 ? s : '0' + s;
+    const html = `${h} : ${minutes} : ${seconds}`;
+    clock.innerHTML = html;
+
+};
+
+const currentDate = () => {
+
+
+    let options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    };
+
+    const fullDate = getDate.toLocaleDateString('en-AUS', options);
+
+
+
+    todaysDate.innerHTML = fullDate;
+
+};
+
+setInterval(tick, 1000);
+
+currentDate();
